2ad4d9b9fc5f81f595805b5e9af9b10279be8923
이종학 <jonghak@jonghak-MacBookPro.local>
2021-06-14 12:48:18 +0900

    App health check endpoint 추가

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/healthz', (req, res) => res.send('App is working'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
